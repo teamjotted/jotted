b4bcc68a4f82ab3bbc3bfef8407aa1ff1afd84da
TalbertH <104466295+TalbertH@users.noreply.github.com>
2023-03-01 18:09:04 -0700

    fixed url

---
+++ src/pages/api/preview.js
@@ -0,0 +1,17 @@
+const urlMetadata = require("url-metadata");
+
+export default async function handler(req, res) {
+  console.log(req.body)
+
+  try {
+    const response = await urlMetadata(req.body).then((res) => {
+      console.log(res);
+      return res
+    });
+    console.log(response)
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json({ error: "failed to create url!" });
+  }
+
+}
